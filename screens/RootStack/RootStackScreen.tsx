670ae7256acb6cc2e2c866eaae8d79de74bbe446
Napasorn Saengthongsrikamol <napasorn.s@berkeley.edu>
2021-04-25 20:39:57 -0700

    new bet screen except dropdown list to add friends

--- screens/RootStack/RootStackScreen.tsx
+++ screens/RootStack/RootStackScreen.tsx
@@ -1,62 +1,70 @@
-import React, { useState, useEffect } from "react";
-import { Platform, View, Text } from "react-native";
-import { Appbar, TextInput, Snackbar, Button } from "react-native-paper";
-import firebase from "firebase/app";
-import "firebase/firestore";
+// import React, { useState, useEffect } from "react";
+// import { Platform, View, Text } from "react-native";
+// import { Appbar, TextInput, Snackbar, Button } from "react-native-paper";
+// import firebase from "firebase/app";
+// import "firebase/firestore";
 
-import DateTimePickerModal from "react-native-modal-datetime-picker";
-import * as SearchableDropdown from "react-native-searchable-dropdown";
+// import DateTimePickerModal from "react-native-modal-datetime-picker";
+// import * as SearchableDropdown from "react-native-searchable-dropdown";
 
-// use bottom tab navigator here
+// // use bottom tab navigator here
 
-export default function RootStackScreen() {
-  const currentUserId = firebase.auth().currentUser!.uid;
-  const [bet_name, setBetName] = useState("");
-  //   const [bet_desc, setBetDesc] = useState("");
-  //   const [bet_type, setBetType] = useState("");
-  //   //   const creator = currentUserId;
-  //   const [friends, setFriends] = useState<[]>();
-  //   const [date_start, setDateStart] = useState();
-  //   const [date_end, setDateEnd] = useState();
-  //   const [evidence, setEvidence] = useState();
-  //   const [invited_users, setInvitedUsers] = useState();
-  //   const [approved_users, setApprovedUsers] = useState();
-  //   const [status, setStatus] = useState("Pending");
-  //   const [wager, setWager] = useState("");
-  //   const [wager_quan, setWagerQuan] = useState("");
-  //   const obj = firebase.firestore().collection("users").doc(currentUserId);
-  //   obj.get().then((doc: any) => {
-  //     setFriends(doc.data().friends);
-  //     setBetName(doc.data().firstName);
-  //   });
+// export default function RootStackScreen() {
+//   const currentUserId = firebase.auth().currentUser!.uid;
+//   //   const [bet_name, setBetName] = useState("");
 
-  //   // Date picker.
-  //   const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
-  //   const [visible, setVisible] = useState(false);
-  //   // Snackbar.
-  //   const [message, setMessage] = useState("");
-  //   // Loading state for submit button
-  //   const [loading, setLoading] = useState(false);
+//   const Bar = () => {
+//     return (
+//       <Appbar.Header>
+//         <Appbar.Action
+//           icon="exit-to-app"
+//           onPress={() => firebase.auth().signOut()}
+//         />
+//         <Appbar.Content title="New Bets" />
+//       </Appbar.Header>
+//     );
+//   };
 
-  const Bar = () => {
-    return (
-      <Appbar.Header>
-        <Appbar.Action
-          icon="exit-to-app"
-          onPress={() => firebase.auth().signOut()}
-        />
-        <Appbar.Content title="New Bets" />
-      </Appbar.Header>
-    );
-  };
+//   return (
+//     <>
+//       <Bar />
+//       <View style={{ padding: 20 }}>
+//         <Text> Hello! </Text>
+//         <Text> {currentUserId}</Text>
+//       </View>
+//     </>
+//   );
+// }
 
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+// import { MainStackScreen } from "./MainStack/MainStackScreen";
+import NewBetScreen from "./NewBetScreen/NewBetScreen.main";
+import { NavigationContainer } from "@react-navigation/native";
+
+export type RootStackParamList = {
+  Main: undefined;
+  NewBetScreen: undefined;
+};
+
+const RootStack = createStackNavigator<RootStackParamList>();
+
+export function RootStackScreen() {
+  const options = { headerShown: false };
   return (
-    <>
-      <Bar />
-      <View style={{ padding: 20 }}>
-        <Text> Hello I am Air HI Hi .</Text>
-        <Text> {currentUserId}</Text>
-      </View>
-    </>
+    <NavigationContainer>
+      <RootStack.Navigator mode="modal" initialRouteName="Main">
+        {/* <RootStack.Screen
+          name="Main"
+          component={MainStackScreen}
+          options={options}
+        /> */}
+        <RootStack.Screen
+          name="NewBetScreen"
+          options={options}
+          component={NewBetScreen}
+        />
+      </RootStack.Navigator>
+    </NavigationContainer>
   );
 }
